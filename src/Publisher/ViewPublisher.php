4a792353fbad6745814e1c8f6ed33d2aa38b9350
Brian Faust <hello@brianfaust.de>
2017-08-02 12:08:16 +0300

    adjustments

--- src/Publisher/ViewPublisher.php
+++ src/Publisher/ViewPublisher.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 /*
  * This file is part of Laravel Service Provider.
  *
- * (c) Brian Faust <hello@brianfaust.de>
+ * (c) Brian Faust <hello@brianfaust.me>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
